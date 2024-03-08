b3f4b535594277cfedb20cc9bf446642cc96402a
AndrewSmooth <gladkiy.a2004@gmail.com>
2024-03-08 21:16:15 +0300

    6-lesson-changes-confirm

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import asyncio
 import os
 
@@ -31,44 +30,3 @@ async def main():
     # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
     await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
-
-=======
-import asyncio
-import os
-
-from aiogram import Bot, Dispatcher
-from aiogram import types
-from aiogram.enums import ParseMode
-
-from dotenv import find_dotenv, load_dotenv
-load_dotenv(find_dotenv())
-
-from handlers.user_private import user_private_router
-from handlers.user_group import user_group_router
-from common.bot_cmds_list import private
-
-ALLOWED_UPDATES = ['message, edited_message']
-
-bot = Bot(token=os.getenv('TOKEN'), parse_mode=ParseMode.HTML)
-
-
-dp = Dispatcher() #Этот класс обрабатывает все события
-
-dp.include_router(user_private_router)
-dp.include_router(user_group_router)
-
-
-
-
-
-
-async def main():
-    await bot.delete_webhook(drop_pending_updates=True)
-    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
-    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
-
-
-
-
->>>>>>> 8cf1ffb (write 5 lesson)
-asyncio.run(main())
